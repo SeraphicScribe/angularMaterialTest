e13a8ecd51a2be1e0cafbb9ad30b99c45f7c43f4
SeraphicScribe <channaka.developer@gmail.com>
2017-01-15 22:34:11 +0530

    Added a custom service

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
     'myApp.view3',
     'myApp.version',
     'ngMaterial',
-    'users'
+    'users',
+    'myservice'
 ])
     .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
